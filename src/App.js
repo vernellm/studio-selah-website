ba32fbb4c8512e4b4be68d1974381305f3da2fdb
vernellm <vtmangum@gmail.com>
2024-02-23 11:53:10 -0600

    added Boostrap and navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      {/* NavBar Header */}
+      <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Studio Selah</span>
+
+        <ul class="navbar-nav flex-row ml-auto">
+          <li class="nav-item active px-2">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item px-2">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item px-2">
+            <a class="nav-link" href="#">Booking</a>
+          </li>
+          <li class="nav-item px-2">
+            <a class="nav-link" href="#">Education</a>
+          </li>
+        </ul>
+      </nav>
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
