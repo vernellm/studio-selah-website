556403da88bcb519a7fd1f8cacd5227be0145f04
vernellm <vtmangum@gmail.com>
2024-02-27 10:26:53 -0600

    updated navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,27 @@
 export default function NavBar() {
     return (
-        <nav class="navbar navbar-light bg-light">
-            <span class="navbar-brand mb-0 h1">Studio Selah</span>
+        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
 
-            <ul class="navbar-nav flex-row ml-auto">
-            <li class="nav-item active px-2">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item px-2">
-                <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item px-2">
-                <a class="nav-link" href="#">Booking</a>
-            </li>
-            <li class="nav-item px-2">
-                <a class="nav-link" href="#">Education</a>
-            </li>
-            </ul>
-      </nav>
+            <div class="container-fluid">
+
+                <span class="navbar-brand mb-0 h1">Studio Selah</span>
+
+                <ul class="navbar-nav flex-row ml-auto">
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#">Booking</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#">Education</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
     );
 }
